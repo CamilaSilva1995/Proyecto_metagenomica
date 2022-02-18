8ea5836acc1d1dea8ce412a06e989beeccfa85b3
Camila Silva <silvagomezpcamila@gmail.com>
2022-02-17 18:25:33 -0600

    clasificador

--- Proyecto_metanomica.py
+++ Proyecto_metanomica.py
@@ -1,127 +1,72 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
 # # Proyecto comparación de Outputs de clasificación taxonómica
 
-# In[2]:
-
-
+import argparse
 import pandas as pd 
 import numpy as np
 
-
-# In[3]:
-
-
 # lectura de los datos por medio de pandas
 # Se tomaron las 10 primeras lineas de el output de kraken
-datakraken10 = pd.read_csv('data/10lineas_kraken.tsv',sep='\t',header=None, names=["c-clasificado", "read_ID","taxon_ID_kraken","length_pb","LCA_taxonomic"])
+datakraken10 = pd.read_csv('data/10lineas_kraken.tsv',sep='\t',header=None, names=["c-clasificado", "read_ID","tax_ID_kraken","length_pb","LCA_taxonomic"])
 #datakraken10 = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRHYe0R7d5WYmKNbyo13egj8ORiKqWwI1GEVaG5Nzp7xN3RjkIV6vnHUweKYpidborNBf4-IEWbk0ZD/pub?output=csv',header=None, names=["c-clasificado", "read","taxon_kraken","1","2"])
 # Se tomaron las 10 primeras lineas de el output de kaiju
-datakaiju10 = pd.read_csv('data/10lineas_kaiju.tsv',sep='\t',header=None, names=["c-clasificado", "read_ID","taxon_ID_kaiju"])
+datakaiju10 = pd.read_csv('data/10lineas_kaiju.tsv',sep='\t',header=None, names=["c-clasificado", "read_ID","tax_ID_kaiju"])
 #datakaiju10 = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRuQ78ZkMRkMz61t0mbMBf4GGsd1ydGxXCl7p2Y8UzgXflkK6w4RCr6lPWXc9oxUXq94qeUJKouCMHp/pub?output=csv',header=None, names=["c-clasificado", "read","taxon_kaiju"])
+
 # Se tomaron las 10 ultimas lineas de el output de kraken
-datakraken10c = pd.read_csv('data/10lineascola_kraken.tsv',sep='\t',header=None, names=["c-clasificado", "read_ID","taxon_ID_kraken","length_pb","LCA_taxonomic"])
+datakraken10c = pd.read_csv('data/10lineascola_kraken.tsv',sep='\t',header=None, names=["c-clasificado", "read_ID","tax_ID_kraken","length_pb","LCA_taxonomic"])
 #datakraken10c = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQJ7psycVzaws-XI_z1itUnxCI3eXBGqK4q57B4gE1YYCVCn_GkvNUZClHPMtrHANy3JUIDpqaW_r0u/pub?output=csv',header=None, names=["c-clasificado", "read","taxon_kraken2","1","2"])
 # Se tomaron las 10 ultimas lineas de el output de kaiju
-datakaiju10c = pd.read_csv('data/10lineascola_kaiju.tsv',sep='\t',header=None, names=["c-clasificado", "read_ID","taxon_ID_kaiju2"])
+datakaiju10c = pd.read_csv('data/10lineascola_kaiju.tsv',sep='\t',header=None, names=["c-clasificado", "read_ID","tax_ID_kaiju2"])
 #datakaiju10c = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vQ-etvzQ8vKI0Xwggc_RvqWboa39GcRdMBw_dSa4-_mx5k9PvaMtfuaqUGvpBM3zUKLDbVkiJ3udybC/pub?output=csv',header=None, names=["c-clasificado", "read","taxon_kaiju2"])
 
-
-# identificar tipo de datos**(Kraken o Kaiju) para automatizar el In3
-
-# In[4]:
-
-
-datakraken10
-
-
-# In[3]:
-
+# identificar tipo de datos**(Kraken o Kaiju) para automatizar
 
 # Eliminamos las columnas no necesarias
-datakraken101 = datakraken10.drop(["length_pb","LCA_taxonomic"], axis=1)
-#datakaiju101 = datakaiju10.drop(['c-clasificado'], axis=1)
-datakaiju101 = datakaiju10
-datakraken102 = datakraken10c.drop(["length_pb","LCA_taxonomic"], axis=1)
-#datakaiju102 = datakaiju10c.drop(['c-clasificado'], axis=1)
-datakaiju102 = datakaiju10c
+datakraken101 = datakraken10.drop(["c-clasificado","length_pb","LCA_taxonomic"], axis=1)
+datakaiju101 = datakaiju10.drop(["c-clasificado"], axis=1)
+
+datakraken102 = datakraken10c.drop(["c-clasificado","length_pb","LCA_taxonomic"], axis=1)
+datakaiju102 = datakaiju10c.drop(["c-clasificado"], axis=1)
 
 # Funcion para los parametros de entrada
 
-# In[4]:
-
-
 lOutputs = [datakraken101, datakaiju101, datakraken102, datakaiju102] # lista de dataframes
+
 # Funcion para unir los outputs en un solo dataframe, tomando como referencia la columna 'read'
-def function(lOutputs):
+def unite(lOutputs):
     DFfusion = lOutputs[0]
     for i in lOutputs:
         DFfusion = pd.merge(DFfusion, i, how='outer')
-   
     return DFfusion
 
-DFfusion = function(lOutputs)    
-DFfusion
+DFfusion = unite(lOutputs)    
 
+### Tomando la totalidad de los datos
 
-# ## Tomando la totalidad de los datos
+datakraken = pd.read_csv('data/21_10_datos_completos/EG1BMAA_kraken.tsv',sep='\t',header=None, names=["c-clasificado", "read_ID","tax_ID_kraken","length_pb","LCA_taxonomic"]) 
+datakaiju = pd.read_csv('data/21_10_datos_completos/EG1BMAA_kaiju.tsv',sep='\t',header=None, names=["c-clasificado","read_ID","tax_ID_kaiju"])
 
-# In[5]:
-
-
-datakraken = pd.read_csv('data/21_10_datos_completos/EG1BMAA_kraken.tsv',sep='\t',header=None, names=["c-clasificado", "read_ID","taxon_ID_kraken","length_pb","LCA_taxonomic"]) 
-datakaiju = pd.read_csv('data/21_10_datos_completos/EG1BMAA_kaiju.tsv',sep='\t',header=None, names=["c-clasificado","read_ID","taxon_ID_kaiju"])
-
-
-# In[6]:
-
-
-datakrakenall = datakraken.drop(["length_pb","LCA_taxonomic"], axis=1)
-datakaijuall = datakaiju
-
-
-# In[7]:
-
+datakrakenall = datakraken.drop(["c-clasificado","length_pb","LCA_taxonomic"], axis=1)
+datakaijuall = datakaiju.drop(["c-clasificado"], axis=1)
 
 allOutputs = [datakrakenall, datakaijuall] #lista de dataframe
-DFfusion = function(allOutputs)    
-DFfusion
+DFfusion = unite(allOutputs)    
 
+DFfusion['measure'] = np.where((DFfusion["tax_ID_kraken"] == DFfusion["tax_ID_kaiju"]) , 1, np.nan)
 
-# In[8]:
+DFfusion.to_csv("clasificador.tsv", sep="\t", index=False, header=True)
 
+        
+#parser = argparse.ArgumentParser()
 
-DFfusion['measure'] = np.where((DFfusion['taxon_kraken'] == DFfusion['taxon_kaiju']) , 1, np.nan)
-DFfusion
+#parser.add_argument("-r","--read",type=str, help="read",required=True)
+#parser.add_argument("-i","--pocision_inicial",type=int, help="pocision inicial", required=True)
+#parser.add_argument("-n","--longitud_final",type=int, help="longitud del read", required=True)
 
+#args = parser.parse_args()
 
-# In[11]:
-
-
-#import argparse, os               
-#import pandas as pd 
-#def get_full_lineages(otus):
-
-	#### makes the full lineage file (lineages.tsv). Requires ete3 ####
-
-	#Input: list of the otus in the table obtained from the get_otus function
-	#Output: makes the full_lineages.tsv file 
-    
-#    from ete3 import NCBITaxa
-#    ncbi = NCBITaxa()
-#    
-#    lineages = {}
-#    
-#    print("hello")
-
-
-# In[13]:
-
-
-#llamar la funcion....
-import get_abundances.py
-
-
-
+#print()
 
--- cutout.py
+++ cutout.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-# ## Funcion para recortar reads
-
-#import argparse
-
+### Funcion para recortar reads
 
 # Funcion cutout(string a recortar, pocision de inicio, longitud del recorte)
 def cutout(read,i,n_length): 
@@ -12,15 +9,21 @@ def cutout(read,i,n_length):
     return(cropped_read)
 
 # ejemplo  
-cropped_read = cutout("CCGTTCCTCGGGCGTGCAGTCGGGCTTGCGGTCTGCCATGTCGTGTTCGGCGTCGGTGGTGCCGATCAGGGTGAAATCCGTCTCGTAGGGGATCGCGAAGATGATCCGCCCGTCCGTGCCCTGAAAGAAATAGCACTTGTCAGATCGGAAGAGCACACGTCTGAACTCCAGTCACCTCAGAATCTCGTATGCCGTCTTCTGCTTGAAAAAAAAAAAAGCAAACCTCTCACTCCCTCTACTCTACTCCCTT",25,10)
-cropped_read
+#cropped_read = cutout("CCGTTCCTCGGGCGTGCAGTCGGGCTTGCGGTCTGCCATGTCGTGTTCGGCGTCGGTGGTGCCGATCAGGGTGAAATCCGTCTCGTAGGGGATCGCGAAGATGATCCGCCCGTCCGTGCCCTGAAAGAAATAGCACTTGTCAGATCGGAAGAGCACACGTCTGAACTCCAGTCACCTCAGAATCTCGTATGCCGTCTTCTGCTTGAAAAAAAAAAAAGCAAACCTCTCACTCCCTCTACTCTACTCCCTT",25,10)
+#cropped_read
 
+####### Arguments #######
+import argparse
+    
+parser = argparse.ArgumentParser()
 
- ####### Arguments #######
-#parser = argparse.ArgumentParser()
-#parser.add_argument("-r","--read",type=str, help="read")
-#parser.add_argument("-i","--pocision_inicial",type=int, help="pocision inicial")
-#parser.add_argument("-n","--longitud_final",type=int, help="longitud del read")
-#args = parser.parse_args()
+parser.add_argument("-r","--read",type=str, help="read",required=True)
+parser.add_argument("-i","--pocision_inicial",type=int, help="pocision inicial", required=True)
+parser.add_argument("-n","--longitud_final",type=int, help="longitud del read", required=True)
+
+args = parser.parse_args()
+
+print(cutout(args.read, args.pocision_inicial, args.longitud_final))
+
 
 
